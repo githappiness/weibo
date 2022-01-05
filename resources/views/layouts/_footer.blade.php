99255607cff994aefa2d35f3ffd11a8c63e27a3d
vagrant <vagrant@homestead>
2022-01-05 07:39:39 +0000

    Move user model to models folder

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="footer">
-  <img class="brand-icon" src="https://iocaffcdn.phphub.org" >
+  {{-- <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png" > --}}
+  <img class="ui image display-inline-block" style="width:18px;height:18px;margin-top:-5px;" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44">
   <a href="https://laravel-china.org/courses" target=_blank>
   刻意练习，每日精进
   </a>
