04bd4753855d0043a51ae27f2095a752b1289147
vagrant <vagrant@homestead>
2022-01-10 06:52:38 +0000

    中文的错误提示信息

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,11 +1,11 @@
 <footer class="footer">
-  {{-- <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png" > --}}
-  <img class="ui image display-inline-block" style="width:18px;height:18px;margin-top:-5px;" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44">
-  <a href="https://laravel-china.org/courses" target=_blank>
-  刻意练习，每日精进
-  </a>
+    {{-- <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png" > --}}
+    <img class="ui image display-inline-block" style="width:18px;height:18px;margin-top:-5px;" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44">
+    <a href="https://laravel-china.org/courses" target=_blank>
+    刻意练习，每日精进
+    </a>
 
-  <div class="float-right">
-    <a href="{{ route('about') }}" >关于</a>
-  </div>
+    <div class="float-right">
+      <a href="{{ route('about') }}" >关于</a>
+    </div>
 </footer>
